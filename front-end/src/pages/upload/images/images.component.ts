2151c5b4d8f12bfd9dd858aa1147e1b8d131b135
CarleoneSouza <carleonedoom@hotmail.com>
2020-08-27 12:24:36 -0300

    implementing upload image component

--- front-end/src/pages/upload/images/images.component.ts
+++ front-end/src/pages/upload/images/images.component.ts
@@ -13,14 +13,30 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class ImagesComponent implements OnInit {
   action: string;
   localData: any;
+  files: File[] = [];
   @Input() productForm: FormGroup;
 
   constructor(public dialogRef: MatDialogRef<ImagesComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: Product,
-              public productService: ProductService,
-              public snackBar: MatSnackBar) { }
+    @Inject(MAT_DIALOG_DATA) public data: Product,
+    public productService: ProductService,
+    public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.localData = this.data;
+  }
+
+  onSelect(event) {
+    console.log(event);
+    this.files.push(...event.addedFiles);
+  }
+
+  onRemove(event) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
