e0d18ffa949b4ebdecfb199e61ea50dd3da244a6
CarleoneSouza <carleonedoom@hotmail.com>
2020-10-20 10:32:04 -0300

    add some changes

--- front-end/src/pages/product-details/product-details.component.ts
+++ front-end/src/pages/product-details/product-details.component.ts
@@ -1,59 +1,66 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.scss"],
 })
-export class ProductDetailsComponent implements OnInit {
-slideIndex = 0;
-  images =[
-    {number:1, src:'https://www.w3schools.com/howto/img_5terre_wide.jpg'},
-    {number:2, src:'https://www.w3schools.com/howto/img_mountains_wide.jpg'},
-    {number:3, src:'https://www.w3schools.com/howto/img_lights_wide.jpg'},
-    {number:4, src:'https://www.w3schools.com/howto/img_nature_wide.jpg'},
-    {number:5, src:'https://www.w3schools.com/howto/img_snow_wide.jpg'}
-  ]
+export class ProductDetailsComponent implements OnInit, AfterViewInit {
+  slideIndex = 0;
+  slides: string | any[];
+  images = [
+    { number: 1, src: "https://www.w3schools.com/howto/img_5terre_wide.jpg" },
+    {
+      number: 1,
+      src: "https://www.w3schools.com/howto/img_mountains_wide.jpg",
+    },
+    { number: 1, src: "https://www.w3schools.com/howto/img_lights_wide.jpg" },
+    { number: 1, src: "https://www.w3schools.com/howto/img_nature_wide.jpg" },
+    { number: 1, src: "https://www.w3schools.com/howto/img_snow_wide.jpg" },
+  ];
 
-  constructor(private _Activatedroute:ActivatedRoute, private host: ElementRef){ }
-
-
-
+  constructor(private _Activatedroute: ActivatedRoute, private host: ElementRef
+  ) {}
+ 
   ngOnInit() {
-
-     this._Activatedroute.paramMap.subscribe(params => {
-
-     });
-
+    this._Activatedroute.paramMap.subscribe((params) => {});
+    
+  }
+  ngAfterViewInit(): void {
+    this.slides = this.host.nativeElement.getElementsByClassName("show");
+    this.showSlides(1);
   }
 
 
-
- plusSlides(n) {
-  this.showSlides(this.slideIndex += n);
-  console.log('N '+n);
-  console.log(this.slideIndex);
-}
-
-currentSlide(n) {
-  this.showSlides(this.slideIndex = n);
-}
-
-showSlides(n) {
-  let i;
-  const slides = this.host.nativeElement.getElementsByClassName('mySlides');
-
-  if (n > slides.length) {this.slideIndex = 1}
-
-  if (n < 1) {this.slideIndex = slides.length}
-
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
+  plusSlides(n: number) {
+    this.showSlides((this.slideIndex += n));
   }
-  slides[this.slideIndex-1].style.display = 'block';
 
-}
+  currentSlide(n: number) {
+    console.log('Current '+ this.slideIndex);
+    this.showSlides((this.slideIndex = n));
+  }
 
+  showSlides(n: number) { 
 
+    if (n > this.slides.length) {
+      this.slideIndex = 1;
+    }
+
+    if (n < 1) {
+      this.slideIndex = this.slides.length;
+    }
+
+    for (let i = 0; i < this.slides.length; i++) {
+      this.slides[i].style.display = "none";
+    }
+    this.slides[this.slideIndex - 1].style.display = "block";
+  }
 }
